de4610f30f2b1af6d17e6de1d7339365a5fe3ff7
David Montgomery <dhmontgomery@users.noreply.github.com>
2022-06-23 12:44:14 -0500

    Update 6/23

--- R/gfx/vax-doses-age-total.R
+++ R/gfx/vax-doses-age-total.R
@@ -3,9 +3,10 @@ p <- vaccine_1x_age %>%
 	mutate(age = str_remove_all(age, " years")) %>%
 	mutate(report_date = as_date(report_date)) %>%
 	left_join(read_csv(here("data-input/mn-age-pop.csv")) %>%
-			  	filter(NAME == "Minnesota", SEX == 0, AGE != 999, AGE >= 5) %>%
+			  	filter(NAME == "Minnesota", SEX == 0, AGE >= 0, AGE != 999) %>%
 			  	select(age = AGE, POPEST2015_CIV:POPEST2019_CIV) %>% #, pop = POPEST2019_CIV) %>%
-			  	mutate(age_bracket = case_when(age %in% 5:11 ~ "5-11",
+			  	mutate(age_bracket = case_when(age %in% 1:4 ~ "0.5-4",
+			  								   age %in% 5:11 ~ "5-11",
 			  								   age %in% 12:15 ~ "12-15",
 			  								   age %in% 16:17 ~ "16-17",
 			  								   age %in% 18:49 ~ "18-49",
@@ -13,13 +14,14 @@ p <- vaccine_1x_age %>%
 			  								   age >= 65 ~ "65+")) %>%
 			  	group_by(age = age_bracket) %>%
 			  	summarize(across(is.numeric, sum)) %>%
+			  	filter(!is.na(age)) %>%
 			  	transmute(age, pop = (POPEST2015_CIV + POPEST2016_CIV + POPEST2017_CIV + POPEST2018_CIV + POPEST2019_CIV) / 5),
 			  by = "age") %>%
 	mutate(pct_onedose = people_onedose / pop,
 		   pct_complete = people_complete / pop,
 		   pct_boosted = people_boosted / pop) %>%
 	mutate(pct_boosted = case_when(age == "5-11" ~ NA_real_, TRUE ~ pct_boosted)) %>%
-	mutate(age = fct_relevel(age, "5-11", "12-15", "16-17", "18-49", "50-64", "65+")) %>%
+	mutate(age = fct_relevel(age, "0.5-4", "5-11", "12-15", "16-17", "18-49", "50-64", "65+")) %>%
 	filter(report_date == max(report_date)) %>%
 	ggplot(aes(x = fct_rev(age))) +
 	geom_col(aes(y = pct_onedose), fill = covidmn_colors[2]) +
@@ -42,9 +44,9 @@ p <- vaccine_1x_age %>%
 	theme_covidmn() +
 	theme(axis.title = element_blank(),
 		  axis.text.x = element_blank(),
-		  plot.subtitle = element_markdown(),
+		  plot.subtitle = element_markdown(lineheight = 1.1),
 		  legend.position = "none") +
 	labs(title = "Total COVID-19 vaccinations to date by age",
-		 subtitle = "Minnesotans with <span style='color:#56B4E9'>at least one dose</span>, <span style='color:#E69F00'>completed vaccinations</span> & <span style='color:#009E73'>boosters</span>",
+		 subtitle = "Minnesotans who have <span style='color:#56B4E9'>at least one dose</span>, <span style='color:#E69F00'>complete vaccinations</span> and who are<br><span style='color:#009E73'>fully boosted</span>. \"Fully boosted\" older adults must have multiple booster doses.",
 		 caption = caption)
 fix_ratio(p, ratio = "short") %>% image_write(here("images/doses-by-age-pct.png"))
