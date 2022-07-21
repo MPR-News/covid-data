232489b338909ec868eb5178c248d307011659b5
David Montgomery <dhmontgomery@users.noreply.github.com>
2022-07-21 11:22:36 -0500

    Update 7/21
    Dashboard still not updated, I'll get it working eventually

--- R/gfx/case-report-day.R
+++ R/gfx/case-report-day.R
@@ -1,12 +1,15 @@
-p <- case_table %>%
+p <- comp_cases_total %>%
 	filter(report_date == max(report_date)) %>%
+	ungroup() %>%
 	# filter(report_date == as_date("2022-02-15")) %>%
 	# filter(report_date == max(report_date) - 7) %>%
 	transmute(date, newly_reported_cases, pct = newly_reported_cases / sum(newly_reported_cases)) %>%
+	filter(!is.nan(pct)) %>%
 	arrange(desc(date)) %>%
 	rowid_to_column() %>%
-	mutate(display_date = case_when(date >= (max(date, na.rm = TRUE) - 7) ~ format(date, "%a\n%m/%d") %>% as.character(), 
+	mutate(display_date = case_when(date >= (max(date, na.rm = TRUE) - 8) ~ format(date, "%a\n%m/%d") %>% as.character(), 
 									TRUE ~ "Older")) %>%
+	filter(pct > 0) %>%
 	mutate(display_date = fct_reorder(display_date, rowid, .desc = TRUE)) %>%
 	group_by(display_date) %>%
 	summarize(newly_reported_cases = sum(newly_reported_cases),
