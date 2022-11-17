11bcf3bb90cfa3453a6a46344461c0d85f1bb242
David Montgomery <dhmontgomery@users.noreply.github.com>
2022-11-17 11:39:54 -0600

    Update 11/17

--- R/load-data/download-hosp.R
+++ R/load-data/download-hosp.R
@@ -2,7 +2,7 @@ if(!exists("current_report_date")) {source(here("R/scraping-setup.R"))}
 
 hosp_total <- "https://www.health.state.mn.us/diseases/coronavirus/stats/h7day.csv" %>%
 	GET(user_agent(user_agent)) %>% 
-	content(encoding = "UTF-8") %>%
+	content(encoding = "UTF-8", type = "text/csv") %>%
 	mutate(across(contains("date"), anydate)) %>%
 	mutate(report_date = current_report_date) %>%
 	rename("new_hosp" = case_count,
@@ -11,7 +11,7 @@ hosp_total <- "https://www.health.state.mn.us/diseases/coronavirus/stats/h7day.c
 
 hosp_county <- "https://www.health.state.mn.us/diseases/coronavirus/stats/hcounty.csv" %>%
 	GET(user_agent(user_agent)) %>% 
-	content(encoding = "UTF-8") %>%
+	content(encoding = "UTF-8", type = "text/csv") %>%
 	separate(adm_date_mmwr, c("year", "week"), sep = 4, convert = TRUE) %>%
 	mutate(across(contains("date"), mdy)) %>%
 	mutate(report_date = current_report_date) %>%
@@ -21,7 +21,7 @@ hosp_county <- "https://www.health.state.mn.us/diseases/coronavirus/stats/hcount
 
 hosp_age <- "https://www.health.state.mn.us/diseases/coronavirus/stats/hage.csv" %>%
 	GET(user_agent(user_agent)) %>% 
-	content(encoding = "UTF-8") %>%
+	content(encoding = "UTF-8", type = "text/csv") %>%
 	separate(adm_date_mmwr, c("year", "week"), sep = 4, convert = TRUE) %>%
 	mutate(across(contains("date"), mdy)) %>%
 	mutate(report_date = current_report_date) %>%
@@ -31,7 +31,7 @@ hosp_age <- "https://www.health.state.mn.us/diseases/coronavirus/stats/hage.csv"
 
 hosp_sex <- "https://www.health.state.mn.us/diseases/coronavirus/stats/hsex.csv" %>%
 	GET(user_agent(user_agent)) %>% 
-	content(encoding = "UTF-8") %>%
+	content(encoding = "UTF-8", type = "text/csv") %>%
 	separate(adm_date_mmwr, c("year", "week"), sep = 4, convert = TRUE) %>%
 	mutate(across(contains("date"), mdy)) %>%
 	mutate(report_date = current_report_date) %>%
@@ -41,7 +41,7 @@ hosp_sex <- "https://www.health.state.mn.us/diseases/coronavirus/stats/hsex.csv"
 
 hosp_race <- "https://www.health.state.mn.us/diseases/coronavirus/stats/hrace.csv" %>%
 	GET(user_agent(user_agent)) %>% 
-	content(encoding = "UTF-8") %>%
+	content(encoding = "UTF-8", type = "text/csv") %>%
 	separate(adm_date_mmwr, c("year", "week"), sep = 4, convert = TRUE) %>%
 	mutate(across(contains("date"), mdy)) %>%
 	mutate(report_date = current_report_date) %>%
@@ -51,14 +51,14 @@ hosp_race <- "https://www.health.state.mn.us/diseases/coronavirus/stats/hrace.cs
 
 hosp_beds <- "https://www.health.state.mn.us/diseases/coronavirus/stats/hcdsource.csv" %>%
 	GET(user_agent(user_agent)) %>% 
-	content(encoding = "UTF-8") %>%
+	content(encoding = "UTF-8", type = "text/csv") %>%
 	mutate(date = date_parser(date)) %>%
 	mutate(report_date = current_report_date) %>%
 	write_csv(here("data/hosp_beds.csv")) 
 
 hosp_bed_avail <- "https://www.health.state.mn.us/diseases/coronavirus/stats/hcapacity.csv" %>%
 	GET(user_agent(user_agent)) %>% 
-	content(encoding = "UTF-8") %>%
+	content(encoding = "UTF-8", type = "text/csv") %>%
 	janitor::clean_names() %>%
 	mutate(date = mdy(date)) %>%
 	mutate(report_date = current_report_date) %>%
